152bd2f6d0d92917d3cf62534e6653f5af41233d
Gasser_Zayed <gasserzayed.work0@gmail.com>
2025-09-25 21:43:41 +0300

    add config vite file

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+        createAccount: resolve(__dirname, 'public/pages/create_account.html'
+      },
+    },
+  },
+});
